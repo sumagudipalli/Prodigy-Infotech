4fe79bb2c62ae6f79597e973a0a1b9b803e8cb83
sumagudipalli <sumagudipallicme@gmail.com>
2025-02-20 19:29:35 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navbar = document.getElementById("navbar");
+
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 50) {
+            navbar.classList.add("scrolled");
+        } else {
+            navbar.classList.remove("scrolled");
+        }
+    });
+});
 
